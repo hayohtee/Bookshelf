b2ed569f11e49a0c399e5f8d93356c011647116b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-08-24 20:33:17 +0100

    Add okhttp3 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 dependencies {
 
     // Retrofit
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
 
