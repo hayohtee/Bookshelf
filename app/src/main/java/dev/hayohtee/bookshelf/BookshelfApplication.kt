a7a1f12a779970a8d3d35cd746d5b549b9c2086f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-08-25 17:40:33 +0100

    Make container variable not private in BookshelfApplication

--- app/src/main/java/dev/hayohtee/bookshelf/BookshelfApplication.kt
+++ app/src/main/java/dev/hayohtee/bookshelf/BookshelfApplication.kt
@@ -3,8 +3,8 @@ package dev.hayohtee.bookshelf
 import android.app.Application
 import dev.hayohtee.bookshelf.data.BookshelfContainer
 
-class BookshelfApplication: Application() {
-    private lateinit var container: BookshelfContainer
+class BookshelfApplication : Application() {
+    lateinit var container: BookshelfContainer
     override fun onCreate() {
         super.onCreate()
         container = BookshelfContainer()
