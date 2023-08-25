d566d5d2ac0e9bc2ffebeda4ac635f63795b707a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-08-25 17:39:24 +0100

    Replace @Path with @Query in BookshelfApiService

--- app/src/main/java/dev/hayohtee/bookshelf/data/BookshelfApiService.kt
+++ app/src/main/java/dev/hayohtee/bookshelf/data/BookshelfApiService.kt
@@ -2,9 +2,9 @@ package dev.hayohtee.bookshelf.data
 
 import dev.hayohtee.bookshelf.data.model.BookResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BookshelfApiService {
-    @GET("/books/v1/volumes?q={title}")
-    suspend fun searchBooks(@Path("title") title: String): BookResponse
+    @GET("/books/v1/volumes")
+    suspend fun searchBooks(@Query("q") title: String): BookResponse
 }
