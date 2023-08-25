012f8bd884c4f9cc419439ae7bfb7375f7e77168
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-08-25 17:32:52 +0100

    Update BookListScreen

--- app/src/main/java/dev/hayohtee/bookshelf/ui/screen/BookListScreen.kt
+++ app/src/main/java/dev/hayohtee/bookshelf/ui/screen/BookListScreen.kt
@@ -1,10 +1,12 @@
 package dev.hayohtee.bookshelf.ui.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -19,10 +21,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import dev.hayohtee.bookshelf.R
 import dev.hayohtee.bookshelf.data.model.Book
+import dev.hayohtee.bookshelf.data.model.BookInfo
 import dev.hayohtee.bookshelf.ui.theme.BookshelfTheme
 
 @Composable
@@ -47,53 +51,55 @@ fun BookListScreen(uiState: BookListUiState, modifier: Modifier = Modifier) {
 
 @Composable
 fun ErrorScreen(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.error),
+            contentDescription = stringResource(id = R.string.error_message),
+            modifier = Modifier.size(200.dp),
+            contentScale = ContentScale.Crop
+        )
         Text(
             text = stringResource(id = R.string.error_message),
             style = MaterialTheme.typography.labelLarge.copy(
                 color = MaterialTheme.colorScheme.error
             )
         )
-        Image(
-            painter = painterResource(id = R.drawable.error),
-            contentDescription = stringResource(id = R.string.error_message)
-        )
     }
 }
 
 @Composable
-fun BookCard(url: String, title: String, modifier: Modifier = Modifier) {
+fun BookCard(bookInfo: BookInfo, modifier: Modifier = Modifier) {
+    val url = bookInfo.imageLinks.thumbnail.replace("http", "https")
     AsyncImage(
-        model = ImageRequest.Builder(context = LocalContext.current)
+        model = ImageRequest.Builder(LocalContext.current)
             .data(url)
-            .crossfade(true)
             .build(),
-        contentDescription = title,
-        modifier = modifier.aspectRatio(1.5f),
+        contentDescription = bookInfo.title,
         contentScale = ContentScale.Crop,
+        modifier = modifier,
         placeholder = painterResource(id = R.drawable.placeholder)
     )
 }
 
 @Composable
 fun BookGrid(books: List<Book>, modifier: Modifier = Modifier) {
-    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+    ) {
         items(items = books, key = { book -> book.id }) { book ->
             BookCard(
-                url = book.info.imageLinks.thumbnail,
-                title = book.info.title
+                bookInfo = book.info,
+                modifier = Modifier.size(200.dp, 300.dp).padding(4.dp)
             )
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun BookCardPreview() {
-    BookshelfTheme {
-        BookCard(url = "", title = "")
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
