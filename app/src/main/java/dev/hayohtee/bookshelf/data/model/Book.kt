92576ee76712d3195a1f3e3409d7dd94d1f6d875
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-08-25 17:32:28 +0100

    Add ImageLink model to Book.kt and add serial name annotation to info field on Book model

--- app/src/main/java/dev/hayohtee/bookshelf/data/model/Book.kt
+++ app/src/main/java/dev/hayohtee/bookshelf/data/model/Book.kt
@@ -1,18 +1,25 @@
 package dev.hayohtee.bookshelf.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Book(
     val id: String,
+    @SerialName("volumeInfo")
     val info: BookInfo
 )
 
 @Serializable
 data class BookInfo(
     val title: String,
-    val subtitle: String,
     val description: String,
     val authors: List<String>,
-    val publishedDate: String
+    val publishedDate: String,
+    val imageLinks: ImageLink
+)
+
+@Serializable
+data class ImageLink(
+    val thumbnail: String
 )
